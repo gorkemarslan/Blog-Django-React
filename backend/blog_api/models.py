05209d4f261ed852d39a0fef5b76d0d9a850e108
grkm <garslan13@gmail.com>
2021-07-30 20:25:05 +0300

    Project schema is designed.

--- backend/blog_api/models.py
+++ backend/blog_api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT, null=True, blank=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=127, unique_for_date='published_at')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    # Default Manager
+    objects = models.Manager()
+    # Custom Manager
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ('-published_at',)
+
+    def __str__(self):
+        return self.title
+
