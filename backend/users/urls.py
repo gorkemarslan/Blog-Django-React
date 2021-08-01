dd397cbad49364f566b1929089c357588b428601
grkm <garslan13@gmail.com>
2021-08-01 19:28:58 +0300

    urls.py in users app is updated.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import CustomUserCreate, BlacklistTokenUpdateView
 
-app_name = 'users'
-
 urlpatterns = [
-    path('create/', CustomUserCreate.as_view(), name="create_user"),
+    path('register/', CustomUserCreate.as_view(), name="register"),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(),
          name='blacklist')
 ]
